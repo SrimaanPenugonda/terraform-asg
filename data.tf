5a95d911519b8eca442297d1434b5af6ef0bca9a
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-14 10:13:21 +0530

    Autoscaling group - added region for remote-state-file

--- data.tf
+++ data.tf
@@ -1,6 +1,10 @@
 data "aws_ami" "ami" {
   most_recent   = true
   owners        = ["973714476881"]
+  filter {
+    name   = "name"
+    values = ["Centos-7-DevOps-Practice"]
+  }
 }
 
 //this is to connect the VPC information , this can be done by accessing VPC state file which
