6c35aa67417ca0623d8693474cf0b93cf37a9376
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-16 10:34:50 +0530

    Autoscaling group - added region for remote-state-file

--- asg.tf
+++ asg.tf
@@ -27,5 +27,38 @@ resource "aws_lb_target_group" "tg" {
   health_check {
     path                    = var.HEALTH
   }
+}
 
+resource "aws_security_group" "allow_component" {
+  name        = "allow-${var.COMPONENT}-${var.ENV}-sg"
+  description = "Allow components to AMI"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] // allow only VPC and workstation - give VPC Cidr block
+    //and workstation cidr block
+
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] // allow internet
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-${var.COMPONENT}-ami-sg"
+  }
 }
